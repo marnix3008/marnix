05d37926d4b90396a035528b1ec752e36dc98bd3
J.Q. Post <j.q.post@student.tudelft.nl>
2026-03-18 13:09:25 +0100

    Test commit, changes flight altitude in orangeavoider c file from 0.8 to 1.0

--- sw/airborne/modules/orange_avoider/orange_avoider.c
+++ sw/airborne/modules/orange_avoider/orange_avoider.c
@@ -60,7 +60,7 @@ float oa_free_threshold        = 30.f;  // danger score below which a slice is c
 float oa_min_gap_width         = 2.f;   // minimum number of adjacent free slices to form a valid gap
 float oa_max_heading_increment = 5.f;   // maximum heading change per tick [deg]
 float maxDistance              = 0.5f;  // max waypoint displacement [m]
-float flight_altitude          = 0.8f;  // target flight altitude above ground [m]
+float flight_altitude          = 1.0f;  // target flight altitude above ground [m]
 
 // State
 enum navigation_state_t navigation_state = SEARCH_FOR_SAFE_HEADING;
